b4810bbe081c7010617a095a4d0fe6d300c63c65
Rafael Ribeiro <rafael@mppe041435l.mp-pe>
2016-02-16 10:38:17 -0300

    without onbuild...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 if [ "$1" = 'oracle-xe' ]; then
+  if [ ! -f /etc/default/oracle-xe ]; then
+    echo "ORACLE_LISTENER_PORT=1521" > /tmp/XE.rsp
+    echo "ORACLE_HTTP_PORT=8080" >> /tmp/XE.rsp
+    echo "ORACLE_PASSWORD=${ORACLE_PASSWORD-manager}" >> /tmp/XE.rsp
+    echo "ORACLE_CONFIRM_PASSWORD=${ORACLE_PASSWORD-manager}" >> /tmp/XE.rsp
+    echo "ORACLE_DBENABLE=y" >> /tmp/XE.rsp
+    /etc/init.d/oracle-xe configure responseFile=/tmp/XE.rsp
+  fi
   service oracle-xe start
   for f in /docker-entrypoint-initdb.d/*; do
 			case "$f" in
